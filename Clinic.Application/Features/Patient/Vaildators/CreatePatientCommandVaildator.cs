e78e8816d9037a2b311977d3494a1302458a4b00
mahmoud <mayad0801@gmail.com>
2026-07-21 16:02:11 +0300

    wip: save all remaining patient changes

--- Clinic.Application/Features/Patient/Vaildators/CreatePatientCommandVaildator.cs
+++ Clinic.Application/Features/Patient/Vaildators/CreatePatientCommandVaildator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Clinic.Application.Features.Patient.Commands;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Clinic.Application.Features.Patient.Vaildators
 {
-    internal class CreatePatientCommandVaildator
+    public class CreatePatientCommandVaildator : AbstractValidator<CreatePatientCommand>
     {
+        public CreatePatientCommandVaildator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("The Name Is required.");
+
+            RuleFor(x => x.Age)
+                .InclusiveBetween(25, 80)
+                .WithMessage("Patient age is required.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .MaximumLength(100)
+                .EmailAddress()
+                .WithMessage("A valid email is required.");
+        }
     }
 }
