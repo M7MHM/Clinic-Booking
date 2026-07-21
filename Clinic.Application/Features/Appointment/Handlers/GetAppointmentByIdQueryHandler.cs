834887e04b015ac25e9b17c6aac2bd96f0f961e9
mahmoud <mayad0801@gmail.com>
2026-07-21 16:18:26 +0300

    fix: add missing doctor and appointment handlers

--- Clinic.Application/Features/Appointment/Handlers/GetAppointmentByIdQueryHandler.cs
+++ Clinic.Application/Features/Appointment/Handlers/GetAppointmentByIdQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using Clinic.Application.Features.Appointment.Dtos;
+using Clinic.Application.Features.Appointment.Queries;
+using Clinic.Domain.interfaces.repos;
+using MediatR;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clinic.Application.Features.Appointment.Handlers
 {
-    internal class GetAppointmentByIdQueryHandler
+    public class GetAppointmentByIdQueryHandler : IRequestHandler<GetAppointmentByIdQuery, AppointmentDto>
     {
+        private readonly IAppointmentRepo _appointmentRepo;
+        private readonly IMapper _mapper;
+
+        public GetAppointmentByIdQueryHandler(IAppointmentRepo appointmentRepo, IMapper mapper)
+        {
+            _appointmentRepo = appointmentRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<AppointmentDto> Handle(GetAppointmentByIdQuery request, CancellationToken cancellationToken)
+        {
+            var appointment = await _appointmentRepo.GetAppointmentByIdAsync(request.Id);
+            if (appointment == null) throw new Exception($"Appointment with Id {request.Id} was not found.");
+            return _mapper.Map<AppointmentDto>(appointment);
+        }
     }
 }
--- Clinic.Application/Features/Doctor/Handlers/GetDoctorByIdQueryHandler.cs
+++ Clinic.Application/Features/Doctor/Handlers/GetDoctorByIdQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using AutoMapper;
+using Clinic.Application.Features.Doctor.Dtos;
+using Clinic.Application.Features.Doctor.Queries;
+using Clinic.Domain.interfaces.repos;
+using MediatR;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Clinic.Application.Features.Doctor.Handlers
 {
-    internal class GetDoctorByIdQueryHandler
+    public class GetDoctorByIdQueryHandler : IRequestHandler<GetDoctorByIdQuery, DoctorDto>
     {
+        private readonly IDoctorRepo _doctorRepo;
+        private readonly IMapper _mapper;
+
+        public GetDoctorByIdQueryHandler(IDoctorRepo doctorRepo, IMapper mapper)
+        {
+            _doctorRepo = doctorRepo;
+            _mapper = mapper;
+        }
+
+        public async Task<DoctorDto> Handle(GetDoctorByIdQuery request, CancellationToken cancellationToken)
+        {
+            var doctor = await _doctorRepo.GetDoctorByIdAsync(request.Id);
+            if (doctor == null) throw new Exception($"Doctor with Id {request.Id} was not found.");
+            return _mapper.Map<DoctorDto>(doctor);
+        }
     }
 }
